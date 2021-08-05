e49b532fdb016f46a7d64062fb8a55bef5ffb8d3
Andres Oller <oller.andres@gmail.com>
2021-08-05 12:22:04 -0300

    Add photos adapter

--- app/src/main/java/com/androidizate/clase8/MainActivity.kt
+++ app/src/main/java/com/androidizate/clase8/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.androidizate.clase8
 
 import android.content.Context
-import android.content.DialogInterface
 import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.androidizate.clase8.adapters.UserAdapter
+import com.androidizate.clase8.adapters.PhotosAdapter
 import com.androidizate.clase8.api.RestApiClient
-import com.androidizate.clase8.dtos.User
+import com.androidizate.clase8.dtos.Photo
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.String.*
 
-class MainActivity : AppCompatActivity(), Callback<List<User>> {
+class MainActivity : AppCompatActivity() {
     private val restApiClient = RestApiClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,12 +32,9 @@ class MainActivity : AppCompatActivity(), Callback<List<User>> {
 
     private fun createAlert(stringResource: String?) {
         AlertDialog.Builder(this)
-            .setMessage(java.lang.String.format(getString(R.string.error), stringResource))
+            .setMessage(format(getString(R.string.error), stringResource))
             .setTitle(getString(R.string.error))
-            .setPositiveButton(
-                R.string.ok,
-                DialogInterface.OnClickListener { dialogInterface, i -> this@MainActivity.finish() }
-            )
+            .setPositiveButton(R.string.ok) { _, i -> this@MainActivity.finish() }
             .show()
     }
 
@@ -49,16 +46,39 @@ class MainActivity : AppCompatActivity(), Callback<List<User>> {
     }
 
     private fun downloadInfo() {
-        restApiClient.getAllUsers().enqueue(this)
-    }
+        /*
+        restApiClient.getAllUsers().enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (response.isSuccessful) {
+                    recycler.adapter = UserAdapter(response.body()!!)
+                }
+            }
 
-    override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-        if (response.isSuccessful) {
-            recycler.adapter = UserAdapter(response.body()!!)
-        }
-    }
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                createAlert(t.message)
+            }
+        })
+        */
 
-    override fun onFailure(call: Call<List<User>>, t: Throwable) {
-        createAlert(t.message)
+        restApiClient.getAllPhotos().enqueue(object : Callback<List<Photo>> {
+            override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        recycler.adapter = PhotosAdapter(it)
+                    } ?: createAlert("Error loading info")
+                    /* Same as above with let
+                    if (response.body() != null) {
+                        recycler.adapter = PhotosAdapter(it)
+                    } else {
+                        createAlert("Error loading info")
+                    }
+                    */
+                }
+            }
+
+            override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
+                createAlert(t.message)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/androidizate/clase8/adapters/PhotosAdapter.kt
@@ -0,0 +1,42 @@
+package com.androidizate.clase8.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.androidizate.clase8.R
+import com.androidizate.clase8.dtos.Photo
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_photo.view.*
+import java.util.*
+
+/**
+ * Created by andres.oller on 18/08/17.
+ */
+class PhotosAdapter(
+    private val photoList: List<Photo> = ArrayList()
+) : RecyclerView.Adapter<PhotosAdapter.ViewHolder?>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val photo = photoList[position]
+        holder.setInfo(photo)
+    }
+
+    override fun getItemCount(): Int = photoList.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun setInfo(photo: Photo) {
+            itemView.title.text = photo.title
+            Glide.with(itemView.context)
+                .load(photo.url)
+                .dontAnimate()
+                .into(itemView.image)
+        }
+    }
+}
